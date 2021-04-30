7e97c2cdaa38fb46fa4d01cd26611540432b3d56
wyjzll <65002463+wyjzll@users.noreply.github.com>
2021-04-30 10:08:24 -0500

    Update check_classes_in_domesNii.py

--- check_classes_in_domesNii.py
+++ check_classes_in_domesNii.py
@@ -1,5 +1,5 @@
 import nibabel as nb
 import numpy as np
-data = nb.load("/research/Data/ADNI_hdBET/Models_byDavid/statistics/Domes1-5/M__002S0685_20110708073112_401_MT1_m_domes.nii.gz
+data = nb.load("/****/**.nii.gz
                ").get_fdata()
 print(np.unique(data))
